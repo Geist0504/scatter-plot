077dc518269785eb4114789952791ac1c9759ae7
Andrew Gallin <andrewgallin@gmail.com>
2018-10-09 08:09:47 -0400

    Updates

---
+++ precache-manifest.b0ba0690398dc0a0aeea2c031a461c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scatter-plot/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c53b21eef36b7d001ec1",
+    "url": "/scatter-plot/static/js/runtime~main.c53b21ee.js"
+  },
+  {
+    "revision": "a9b101f32601f0a0ba17",
+    "url": "/scatter-plot/static/js/main.a9b101f3.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/scatter-plot/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "a9b101f32601f0a0ba17",
+    "url": "/scatter-plot/static/css/main.61b225b1.chunk.css"
+  },
+  {
+    "revision": "29dd9b8a68e23c21229109f2a2f30a17",
+    "url": "/scatter-plot/index.html"
+  }
+];
